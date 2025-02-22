d161b52f36ea5e000aeccbd727dca76b6fa85d96
Egor <lesin.scl@gmail.com>
2025-02-22 21:21:02 +0300

    Добавил проверку на считывание неверных значений из файла

--- QuadraticEquationRoots.cpp
+++ QuadraticEquationRoots.cpp
@@ -3,23 +3,36 @@
 #include <cmath>
 #include <limits>
 
+bool validateCoefficients(std::ifstream& inputFile, double& a, double& b, double& c) {
+    if (!(inputFile >> a >> b >> c)) {
+        std::cerr << "Error: incorrect data in the file" << std::endl;
+        return false;
+    }
+    return true;
+}
+
 void readCoefficients(const std::string& filename, double& a, double& b, double& c) {
     std::ifstream inputFile(filename);
     if (!inputFile) {
-        std::cerr << "Error when opening a file!" << std::endl;
+        std::cerr << "File opening error" << std::endl;
         exit(1);
     }
-    inputFile >> a >> b >> c;
+    if (!validateCoefficients(inputFile, a, b, c)) {
+        exit(1);
+    }
     inputFile.close();
 }
 
-void solveEquation(double a, double b, double c) {
+void SolveEquation(double a, double b, double c) {
     if (a == 0) {
         if (b == 0) {
             std::cout << "The equation has no solutions" << std::endl;
         }
+        else if (c == 0) {
+            std::cout << "It's a linear equation, one root: " << 0 << std::endl;
+        }
         else {
-            std::cout << "It's a linear equation, one root :" << static_cast<double>(-c) / b << std::endl;
+            std::cout << "It's a linear equation, one root: " << - c / b << std::endl;
         }
         return;
     }
@@ -28,7 +41,7 @@ void solveEquation(double a, double b, double c) {
     if (D > 0) {
         double x1 = (-b + std::sqrt(D)) / (2 * a);
         double x2 = (-b - std::sqrt(D)) / (2 * a);
-        std::cout << "The roots of the equation: " << x1 << " и " << x2 << std::endl;
+        std::cout << "The roots of the equation: " << x1 << " and " << x2 << std::endl;
     }
     else if (D == 0) {
         double x = -b / (2 * a);
@@ -43,6 +56,6 @@ int main() {
     double a, b, c;
     std::string filename = "input.txt";
     readCoefficients(filename, a, b, c);
-    solveEquation(a, b, c);
+    SolveEquation(a, b, c);
     return 0;
 }
