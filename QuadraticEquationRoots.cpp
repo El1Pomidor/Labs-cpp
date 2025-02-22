44925810911c3d7d9b39326da71c47faaa508530
Egor <lesin.scl@gmail.com>
2025-02-22 20:24:37 +0300

    Добавил считывание из файла и разделил на функции

--- QuadraticEquationRoots.cpp
+++ QuadraticEquationRoots.cpp
@@ -1,41 +1,48 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <cmath>
+#include <limits>
+
+void readCoefficients(const std::string& filename, double& a, double& b, double& c) {
+    std::ifstream inputFile(filename);
+    if (!inputFile) {
+        std::cerr << "Error when opening a file!" << std::endl;
+        exit(1);
+    }
+    inputFile >> a >> b >> c;
+    inputFile.close();
+}
+
+void solveEquation(double a, double b, double c) {
+    if (a == 0) {
+        if (b == 0) {
+            std::cout << "The equation has no solutions" << std::endl;
+        }
+        else {
+            std::cout << "It's a linear equation, one root :" << static_cast<double>(-c) / b << std::endl;
+        }
+        return;
+    }
+
+    double D = b * b - 4 * a * c;
+    if (D > 0) {
+        double x1 = (-b + std::sqrt(D)) / (2 * a);
+        double x2 = (-b - std::sqrt(D)) / (2 * a);
+        std::cout << "The roots of the equation: " << x1 << " и " << x2 << std::endl;
+    }
+    else if (D == 0) {
+        double x = -b / (2 * a);
+        std::cout << "One root: " << x << std::endl;
+    }
+    else {
+        std::cout << "There are no real roots." << std::endl;
+    }
+}
 
 int main() {
     double a, b, c;
-
-    std::cout << "ax^2 + bx + c = 0" << std::endl;
-    std::cout << "Please, enter coefficients a, b and c " << std::endl;
-    std::cout << "a = ";
-    std::cin >> a ;
-    std::cout << "b = ";
-    std::cin >> b;
-    std::cout << "c = ";
-    std::cin >> c;
-
-    if (a == 0) {
-        if (b == 0) {
-            std::cout << "Equation doesn't have solutions." << std::endl;
-        }
-        else {
-            std::cout << "It's a linear equation, the root: " << c / b << std::endl;
-        }
-    }
-    else {
-        double D = b * b - 4 * a * c;
-        if (D > 0) {
-            double x1 = (- b + std::sqrt(D)) / (2.0 * a);
-            double x2 = (-b - std::sqrt(D)) / (2.0 * a);
-            std::cout << "Roots are: " << x1 << " and " << x2 << std::endl;
-        }
-        else if (D == 0) {
-            double x = -b / (2.0 * a);
-            std::cout << "There is only one root: " << x << std::endl;
-        }
-        else {
-            std::cout << "There are no real roots." << std::endl;
-        }
-    }
-
+    std::string filename = "input.txt";
+    readCoefficients(filename, a, b, c);
+    solveEquation(a, b, c);
     return 0;
 }
